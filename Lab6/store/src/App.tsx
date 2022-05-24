513f873445129cee8aab118c0f49e1541120ab38
Evanesa Tuceac <nothing33352@gmail.com>
2022-05-24 22:44:27 +0300

    Lab6

---
+++ Lab6/store/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import './App.css'
+
+import { Layout } from 'antd'
+import { useRootStore } from './index'
+import { HeaderLayout } from './Layouts/HeaderLayout'
+import { ContentLayout } from './Layouts/ContentLayout'
+
+function App() {
+
+    const rootStore = useRootStore()
+
+    useEffect(() => {
+        rootStore.setInitialStorageContents()
+    }, [])
+
+    return (
+        <><Router>
+            <Switch>
+                <Route exact path="/login" component={login} />
+                <Route
+                    exact
+                    path="/profile"
+                    component={() => <Profile authorized="true" />}>
+
+                </Route>
+            </Switch>
+        </Router><Layout className="layout">
+                <HeaderLayout />
+                <ContentLayout />
+            </Layout></>
+    )
+}
+
+export default App
